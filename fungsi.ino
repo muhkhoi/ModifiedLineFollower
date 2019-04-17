c9d9b153e1b6c9d6008ef346ad6c2e35819f7cb1
muhkhoi <muhkhoi@icloud.com>
2019-04-18 03:32:32 +0700

    Add files via upload

---
+++ fungsi.ino
@@ -0,0 +1,101 @@
+void berhenti()
+{
+  analogWrite(motor, 0);
+
+}
+
+void trace()
+{
+  baca_sensor();
+  calibra();
+  steer = map(kode, -6, 6, sudut_max, sudut_min);
+  if (kode == -6)
+  {
+    while (1)
+    {
+      servosteer.write(sudut_max);
+      analogWrite(motor, 50);
+      baca_sensor();
+      if (sensor == 0b111110 || sensor == 0b111100)
+        break;
+    }
+  }
+ 
+  servosteer.write(steer);
+  analogWrite(motor, 70);
+}
+
+void baca_sensor() {
+  if (analogRead(S0) < ns0) {
+    sensor |= B00000001;
+  }
+  else {
+    sensor &= B11111110;
+  }
+  if (analogRead(S1) < ns1) {
+    sensor |= B00000010;
+  }
+  else {
+
+    sensor &= B11111101;
+  }
+  if (analogRead(S2) < ns2) {
+
+    sensor |= B00000100;
+  }
+  else {
+
+    sensor &= B11111011;
+  }
+  if (analogRead(S3) < ns3) {
+
+    sensor |= B00001000;
+  }
+  else {
+
+    sensor &= B11110111;
+  }
+  if (analogRead(S4) < ns4) {
+
+    sensor |= B00010000;
+  }
+  else {
+
+    sensor &= B11101111;
+  }
+  if (analogRead(S5) < ns5) {
+
+    sensor |= B00100000;
+  }
+  else {
+
+    sensor &= B11011111;
+  }
+}
+
+void calibra()
+{
+  switch (sensor)
+  { 
+    case 0b011111: kode = 5; break;
+    case 0b001111: kode = 4; break;
+    case 0b101111: kode = 3; break;
+    case 0b100111: kode = 2; break;
+    case 0b110111: kode = 1; break;
+    case 0b110011: kode = 0; break;
+    case 0b111011: kode = -1; break;
+    case 0b111001: kode = -2; break;
+    case 0b111101: kode = -3; break;
+    case 0b111100: kode = -4; break;
+    case 0b111110: kode = -5; break;
+    case 0b111111:
+      if    (kode < 0) {
+        kode = -6;
+      }
+      else  {
+        kode = 6;
+      }
+      break;
+  }
+}
+
---
+++ main.ino
@@ -0,0 +1,75 @@
+#include <Servo.h>
+
+#define S0 A0
+#define S1 A1
+#define S2 A2
+#define S3 A3
+#define S4 A4
+#define S5 A5
+//int ns0=40,ns1=40,ns2=40,ns3=40,ns4=40,ns5=40;// truk 1
+int ns0 = 40, ns1 = 40, ns2 = 40, ns3 = 70, ns4 = 70, ns5 = 70; // truk 1
+//int ns0=50,ns1=40,ns2=40,ns3=40,ns4=40,ns5=50;// truk 2
+//int ns0=40,ns1=40,ns2=40,ns3=40,ns4=40,ns5=40; //--> 180-0// truk 3
+//int ns0=140,ns1=45,ns2=45,ns3=45,ns4=45,ns5=45; //--> 180-0// truk 4
+
+byte sensor = 0;
+int kode;
+int steer;
+int rr;
+int motor = 3;
+
+int sudut_max = 179;
+int sudut_min = 0;
+
+Servo servosteer;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  servosteer.attach(6);
+  pinMode(motor, 1);
+  
+  for (rr = 0; rr <= 2; rr++)
+  {
+    baca_sensor();
+    calibra();
+    while (sensor != 0b000000)
+    {
+      baca_sensor();
+      calibra();
+      trace();
+    }
+    berhenti();
+    delay(5000);
+  }
+  berhenti();
+  delay(5000);
+}
+
+void loop() {
+  //for(rr=0;rr<=5;rr++){Serial.print(analogRead(rr));Serial.write('\t');}Serial.write('\n');
+  // baca_sensor();
+  //Serial.println(sensor,BIN);
+  //steer=map(kode,-6,6,180,0);
+  //servosteer.write(steer);
+  /*
+  if(kode==-6)
+  {
+    while(1)
+    {
+      servosteer.write(180);
+      analogWrite(motor,125);
+      baca_sensor();
+      if(sensor==0b111110 || sensor==0b111100)
+      break;
+      }
+    }
+
+  Serial.print(kode);Serial.write('\t');
+  Serial.print(steer);Serial.write('\n');
+
+  analogWrite(motor,125);
+
+  */
+}
+
+
